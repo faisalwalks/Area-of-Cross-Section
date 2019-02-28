cb8c6c4286061577c3495f251256bd792705e96b
faisalwalks <48072468+faisalwalks@users.noreply.github.com>
2019-02-27 23:11:44 -0700

    intial commit

---
+++ Schwarz D/Version 1/V1/BMP Files/Surface_Area.m
@@ -0,0 +1,16 @@
+clc
+srcFile=dir('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Schwarz D\V1\BMP Files\*.bmp');
+percentageBlack=zeros(1,length(srcFile));
+sum=0;
+for i=1:length(srcFile)
+    filename=strcat('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Schwarz D\V1\BMP Files\',srcFile(i).name);
+    I=imread(filename);
+%     path=strcat('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Schwarz D\V1\BMP Files\save\',srcFile(i).name);
+%     imwrite(I,path);
+    percentageBlack(1,i)=((1-nnz(I)/numel(I)));
+    sum=sum+percentageBlack(1,i);
+end
+
+sum_avg=sum/length(srcFile)
+area=sum_avg*70*70
+    
---
+++ Spherical Gyroid/V2 V2/BMP Files/Surface_Area.m
@@ -0,0 +1,19 @@
+clc
+% srcFile=dir('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Spherical Gyroid\V3 V1\BMP Files\*.bmp');
+% percentageBlack=zeros(1,length(srcFile));
+% sum=0;
+% for i=1:length(srcFile)
+%     filename=strcat('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Spherical Gyroid\V3 V1\BMP Files\',srcFile(i).name);
+%     I=imread(filename);
+% %     path=strcat('H:\Google Drive\College Stuff\Studies\Thesis (Project)\Models\Schwarz D\V1\BMP Files\save\',srcFile(i).name);
+% %     imwrite(I,path);
+%     percentageBlack(1,i)=((1-nnz(I)/numel(I))*100);
+%     sum=sum+percentageBlack(1,i);
+% end
+
+I=imread('Spherical Gyroid V2 (repaired)_485.bmp')
+ percentageBlack=((1-nnz(I)/numel(I)))
+ area=percentageBlack*70*70
+% sum_avg=sum/length(srcFile)
+% area=sum_avg*70*70
+    
